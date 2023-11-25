b3ba8539283cc1a445c49a24ae6c73ed57ac0fa4
saravan <saravanram22@gmail.com>
2023-11-25 20:58:24 +0530

    added user profile and main containers

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script>
-import topbar from './components/topbar.vue';
+import topbar from './components/topbar.vue'; 
 
 export default {
     name: 'App', 
@@ -10,7 +10,17 @@ export default {
 </script>
 
 <template>
-  <topbar></topbar>
+  <div class="main-wrapper">
+    <div class="sidebar">
+      <div class="user-profile">
+        <img src="" alt="">
+        <h4>Saravan Kumar</h4>
+      </div>
+    </div>
+    <div class="main-container">
+      
+    </div>
+  </div>
 </template>
 
 <style scoped>
